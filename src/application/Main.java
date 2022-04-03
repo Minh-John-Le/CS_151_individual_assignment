7bb68f24e54bdd5e517592c960d81b9c290e996f
Minh-John-Le <99804254+Minh-John-Le@users.noreply.github.com>
2022-04-03 01:49:24 -0700

    Complete Asignment

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,34 +9,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 
 public class Main extends Application {
-	private String title = "ODBC Microsoft Acces Setup";
-	
-	private String cssLayout = "-fx-border-color: white;\n" +
-            "-fx-border-insets: 5;\n" +
-            "-fx-border-width: 1;\n" +
-            "-fx-border-style: solid;\n";
+	private String title = "ODBC Microsoft Access Setup";
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			
-
-			
-			GridPane root = new GridPane();
-			
+		
+			//===============================================================
 			// Input data Pane
 			GridPane inputDataPane = new GridPane();
 			
@@ -55,69 +43,141 @@ public class Main extends Application {
 			inputDataPane.add(description, 0,1);
 			inputDataPane.add(descriptionText, 1,1);
 			
-			
+			//===============================================================
 			// Label Pane
-			StackPane stackPanelMiddle = new StackPane();
 			
 			GridPane labelPanel = new GridPane();
 			labelPanel.setPadding(new Insets(10, 10, 10, 10));
+	
 			
-			Label databaseCaptionLbl = new Label("Database");
+
+		
 			Label databaseLbl = new Label("Database: ");
-			
 			Button  selectButton = new Button("Select...");
 			Button  createButton = new Button("Create...");
 			Button  repairButton = new Button("Repair...");
 			Button  compactButton = new Button("Compact...");
 			
+			labelPanel.setHgap(15);
 			labelPanel.add(databaseLbl, 0, 0);
 			labelPanel.add(selectButton, 0, 1);
 			labelPanel.add(createButton, 1, 1);
 			labelPanel.add(repairButton, 2, 1);
 			labelPanel.add(compactButton, 3, 1);
 			
+			
+			StackPane finalLabelPanel = CreateBorderAndTile(labelPanel, "Database");
+			
+			//==============================================================
+			//Putting Input data Pane and Label Pane  and left side button together
+			
+			VBox dataPane_LabelPane = new VBox();
+			
+			dataPane_LabelPane.getChildren().addAll(inputDataPane, finalLabelPanel);
+			
+			//==============================================================
+			// Left side button
+			VBox leftSideButtons = new VBox();
+			leftSideButtons.setSpacing(10);
+			leftSideButtons.setPadding(new Insets(10, 10, 10, 20));
+			
+			Button OKbutton = new Button("OK");
+			OKbutton.setPrefWidth(80);
+			Button Cancelbutton = new Button("Cancel");
+			Cancelbutton.setPrefWidth(80);
+			Button Helpbutton = new Button("Help");
+			Helpbutton.setPrefWidth(80);
+			Button Advancedbutton = new Button("Advanced...");
+			Advancedbutton.setPrefWidth(80);
+			
+			leftSideButtons.getChildren().addAll(OKbutton,Cancelbutton,Helpbutton,Advancedbutton);
+					
+			//===============================================================
 			// System Data Base Panel
+		
+
+			VBox  systemDataBasePanel = new VBox();
+			RadioButton noneButton = new RadioButton("None");
+			RadioButton databaseButton = new RadioButton("Database");	
 			
+			Button systemDataButton = new Button("System Database..");
+			systemDataButton.setStyle("-fx-translate-x: 100\n;");
 			
-			StackPane bottomLeftPanel = new StackPane();
+			systemDataBasePanel.getChildren().addAll(noneButton, databaseButton, systemDataButton);
+			systemDataBasePanel.setSpacing(10);
 			
-			GridPane systemDataBasePanel = new GridPane();
 			systemDataBasePanel.setPadding(new Insets(10, 10, 10, 10));
 			
-			Label systemDatabaselbl = new Label("System Database");
-			
-			RadioButton noneButton = new RadioButton("None");
-			RadioButton databaseButton = new RadioButton("Database");
-			
-			VBox  noneAndDataBtn = new VBox();
+			StackPane bottomLeftPanel = CreateBorderAndTile(systemDataBasePanel, "System Database");
 			
 			
-
-			noneAndDataBtn.getChildren().addAll(noneButton, databaseButton);
-
-			systemDataBasePanel.setStyle(cssLayout);
-			systemDataBasePanel.add(noneAndDataBtn, 0, 0);
+			//========================================================================
+			// Bottom right button
+			Button optionButton = new Button("Options>>");
 			
-			systemDatabaselbl.setPadding(new Insets(-5, 0, 0, 10));
+			optionButton.setStyle("-fx-translate-x: 20\n;" + "-fx-translate-y: 35\n;");
 			
-			bottomLeftPanel.getChildren().addAll(systemDataBasePanel, systemDatabaselbl);
 			
-			bottomLeftPanel.setAlignment(systemDatabaselbl, Pos.TOP_LEFT);
-			
+			//========================================================================
 			// Root
-			root.add(inputDataPane, 0, 0);
-			root.add(labelPanel, 0, 1);
-			root.add(bottomLeftPanel, 0, 2);
+			GridPane root = new GridPane();
 			
-			Scene scene = new Scene(root,400,400);
+			root.add(dataPane_LabelPane, 0, 0);
+			root.add(leftSideButtons, 1, 0);
+
+			root.add(bottomLeftPanel, 0, 1);
+			root.add(optionButton, 1, 1);
+			
+			root.setStyle("-fx-background-color:#A9A9A9;");
+			Scene scene = new Scene(root, 500, 500);
+			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle(title);
 			primaryStage.show();
+			
+			
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	
+	
+	private StackPane CreateBorderAndTile(Pane pane, String title)
+	{
+		
+		String cssLayout = "-fx-border-color: white;\n" +
+	            "-fx-border-insets: 5;\n" +
+	            "-fx-border-width: 1;\n" +
+	            "-fx-border-style: solid;\n";
+		
+		
+		String textStyle = "-fx-text-fill: black;\n"+   
+				 "-fx-translate-x: 10\n;" +
+				 "-fx-translate-y: -5\n;" +
+				 "-fx-background-color: #A9A9A9;\n";
+
+				
+		Label borderTitle = new Label(title);
+		borderTitle.setStyle(textStyle);
+		
+		pane.setStyle(cssLayout);
+		
+		StackPane result = new StackPane();
+		result.getChildren().addAll(pane, borderTitle);
+		
+		
+	
+		
+		StackPane.setAlignment(borderTitle, Pos.TOP_LEFT);
+
+		
+		return result;
+	}
+			
+			
+			
 	public static void main(String[] args) {
 		launch(args);
 	}
